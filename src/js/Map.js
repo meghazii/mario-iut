9a03a38be7d83aadc080c38e60fad8bbbff97eef
Alezot Josselin <Alezot Josselin>
2017-03-11 20:29:22 +0100

    idk what i am doing

--- src/js/Map.js
+++ src/js/Map.js
@@ -10,14 +10,18 @@ class Map{
 	    this.tiles[i] = new Array(this.heightMapA);
 	}
 	for(var i = 0;i < this.widthMapA;i++){
-	    for(var j = 0;j < 19;j++){
-		if( Math.floor((Math.random() * 50) + 1) == 1 && j < 12){
-		    this.tiles[i][j] = new Entity(32,32,"./data/img/cloud.png", false);
-		}
-		else{
-		    this.tiles[i][j] = new Entity(32,32,"./data/img/ciel.png", false);
-		}
+	    for(var j = 0;j < this.heightMapA - 1;j++){
+			if( Math.floor((Math.random() * 50) + 1) == 1 && j < 12){
+		  	  this.tiles[i][j] = new Entity(32,32,"./data/img/cloud.png", false);
+			}
+			else{
+		    	this.tiles[i][j] = new Entity(32,32,"./data/img/ciel.png", false);
+			}
 	    }
+	}
+
+	for(i = 0; i < this.widthMapA;i++){
+		this.tiles[i][19] = new Entity(32,32,"./data/img/terre.png", false);
 	}
 	
 	this.tiles[4][this.heightMapA - 2] = new Entity(32,32,"./data/img/escalier.png", true);
